dfec985d1c5124718ce65bff52b2921aca30a927
Sergey Gabdullin <s.gabdullin@gmail.com>
2021-03-17 19:10:22 +0500

    Добавил роутинг

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
-import { AboutComponent } from './componets/about/about.component';
+import { AboutComponent } from './components/about/about.component';
+import { from } from 'rxjs';
+
+const appRoutes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   declarations: [
@@ -12,7 +19,8 @@ import { AboutComponent } from './componets/about/about.component';
     AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
